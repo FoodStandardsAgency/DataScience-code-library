fa905c80e9e3abcc3d825fdd4b801b2e13e736e1
xavier-gilbert <xavier.gilbert@food.gov.uk>
2020-11-10 09:48:54 +0000

    rename files to convention

--- geocoder-boilerplate.R
+++ geocoder-boilerplate.R
@@ -13,13 +13,13 @@ register_google(key = 'Your API key')
 geocoded <- data.frame(stringsAsFactors = FALSE)
 
 # Loop through the addresses to get the lat / long data and add to mylocations data frame
-for(i in 1:nrow(mylocations))
-{
+for(i in 1:nrow(mylocations)) {
   # Print("Working...")
   result <- geocode(mylocations$columnName[i], output = "latlona", source = "google")
   mylocations$lon[i] <- as.numeric(result[1])
   mylocations$lat[i] <- as.numeric(result[2])
   mylocations$geoAddress[i] <- as.character(result[3])
 }
+
 # Write a CSV file of your geocoded data to the working directory
 write.csv(mylocations, "geocoded_locations.csv", row.names=FALSE)
